cfdb12109e785d6fb1603efaf8cd3214ad381d18
Robbie <rob.cecchini>
2023-05-20 13:06:34 -0500

    added while loop to force rerun to avoid write to dir errors

--- init_script.sh
+++ init_script.sh
@@ -20,12 +20,15 @@ root_dirs=($(ls / | grep -vE "(dev|proc|sys|run|boot|bin|sbin|usr|lib|etc|root)"
 
 # Get a random root level directory and subdirectory for each file
 for file in netconfig CHANGELOG .sysconfig; do
-  root_dir=${root_dirs[$RANDOM % ${#root_dirs[@]}]}
-  sub_dirs=($(ls /$root_dir))
-  sub_dir=${sub_dirs[$RANDOM % ${#sub_dirs[@]}]}
+  success=0
+  while [ $success -eq 0 ]; do
+    root_dir=${root_dirs[$RANDOM % ${#root_dirs[@]}]}
+    sub_dirs=($(ls /$root_dir))
+    sub_dir=${sub_dirs[$RANDOM % ${#sub_dirs[@]}]}
 
-  # Move the file to the random directory
-  mv $file /$root_dir/$sub_dir/
+    # Try to move the file to the random directory
+    mv $file /$root_dir/$sub_dir/ && success=1
+  done
 
   # Print the location of the file
   echo "File location: /$root_dir/$sub_dir/$file" >> /var/log/flag_planting.log
