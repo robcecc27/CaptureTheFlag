406a9a4d11e025f132b02cb6f7ba8fe4d04ce9cb
Robbie <rob.cecchini>
2023-10-28 11:51:56 -0500

    level.2 initial testing ready

--- init_script.sh
+++ init_script.sh
@@ -5,14 +5,31 @@ exec > >(tee /var/log/user-data.log) 2>&1
 echo "Updating packages"
 yum update -y || exit 1
 
+echo "Installing PHP"
+yum install -y php || exit 1
+
 echo "Installing gcc and httpd"
 yum install gcc httpd -y || exit 1
 
-echo "Creating webpage"
-echo "<html><body><h1>Congratulations, You Found the Webpage! Validation Code = Treefrog</h1></body></html>" >/var/www/html/index.html || exit 1
 
-echo "Starting httpd"
-systemctl start httpd || exit 1
+# Create the webpage using PHP
+cat <<- 'EOF' > /var/www/html/index.php
+<html>
+<body>
+<h1>Congratulations, You Found the Webpage! Validation Code = "Attack or the Kittens"</h1>
+<?php
+  \$files = glob("/var/www/html/*.jpg");
+  foreach (\$files as \$file) {
+    echo '<img src="' . basename(\$file) . '" alt="Kitten">';
+  }
+?>
+</body>
+</html>
+EOF
+
+
+echo "Restarting httpd to apply PHP changes"
+systemctl restart httpd || exit 1
 
 echo "Enabling httpd"
 systemctl enable httpd || exit 1
@@ -61,3 +78,73 @@ for file in netconfig syslog_backup .sysconfig; do
   # Print the location of the file
   echo "File location: $sub_dir$file" >>/var/log/flag_planting.log
 done
+
+# Creating scripts for traffic, memory, disk and DB load
+echo "Creating Traffic Generation Script with random kittens"
+cat <<- 'EOF' > /root/traffic_gen.sh
+#!/bin/bash
+while true; do
+  width=$((100 + RANDOM % 800))
+  height=$((100 + RANDOM % 800))
+  curl -o "/var/www/html/kitten_${width}_${height}.jpg" "http://placekitten.com/${width}/${height}"
+  sleep 10
+done
+EOF
+
+echo "Creating Memory Eating Script"
+cat <<- 'EOF' > /root/memory_eater.py
+import time
+large_list = []
+while True:
+  large_list.append('a' * 10240)
+  time.sleep(1)
+EOF
+
+echo "Creating Disk Eating Script"
+cat <<- 'EOF' > /root/disk_eater.sh
+#!/bin/bash
+while true; do
+  dd if=/dev/zero of=/tmp/large_file bs=1M count=100 oflag=append conv=notrunc
+  sleep 1
+done
+EOF
+
+# Creating MySQL Database Write Script
+echo "Creating MySQL Database Write Script"
+cat <<- 'EOF' > /root/db_writer.py
+import pymysql
+import random
+import time
+
+# Connection details
+db_params = {
+  'host': 'localhost',
+  'user': 'admin-user',
+  'password': 'P@ssWord',
+  'database': 'CaptureTheFlag'
+}
+
+conn = pymysql.connect(**db_params)
+cursor = conn.cursor()
+
+while True:
+  cursor.execute("INSERT INTO table_name (column_name) VALUES (%s)", (random.randint(1, 100000),))
+  conn.commit()
+  time.sleep(1)
+EOF
+
+
+# Make scripts executable
+chmod +x /root/traffic_gen.sh /root/disk_eater.sh
+
+# Schedule scripts with cron
+echo "Scheduling scripts with cron"
+echo "* * * * * /usr/bin/bash /root/traffic_gen.sh" >> mycron
+echo "* * * * * /usr/bin/python /root/memory_eater.py" >> mycron
+echo "* * * * * /usr/bin/bash /root/disk_eater.sh" >> mycron
+echo "* * * * * /usr/bin/python /root/db_writer.py" >> mycron
+crontab mycron
+rm mycron
+
+# All Done
+echo "All tasks completed"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,7 @@
 region = "us-east-1"
 ## EC2 Instance Variables ---------------------
 instance_type         = "t2.micro"
-public_instance_count = 3
+public_instance_count = 1
 ## Networking Variables ------------------------
 vpc_cidr            = "10.0.0.0/16"
 public_subnet_cidr  = "10.0.1.0/24"
